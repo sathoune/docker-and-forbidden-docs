e2270a8a0bc242a5cf8c1db61c41711d9ec2cd8a
sathoune <michal5bargiel@gmail.com>
2023-01-24 22:09:29 +0100

    💄 Highlight current item.

--- components/Commons/Lif.tsx
+++ components/Commons/Lif.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const Lif = ({ children }: { children: ReactNode }) => {
-  return <li className="fragment">{children}</li>;
+  return <li className="fragment fade-in-then-semi-out">{children}</li>;
 };
 
 export default Lif;
