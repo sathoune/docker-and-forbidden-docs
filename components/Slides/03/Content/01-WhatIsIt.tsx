f6113b7b64659388d8046180b05c8d86bbae222f
sathoune <michal5bargiel@gmail.com>
2023-01-25 00:10:32 +0100

    ♻️ Add more points to `about`.

--- components/Slides/03/Content/01-WhatIsIt.tsx
+++ components/Slides/03/Content/01-WhatIsIt.tsx
@@ -6,7 +6,9 @@ const WhatIsIt = () => {
       <h2>What is this Docker thing</h2>
       <ul>
         <Lif>Set of tools enabling containerization</Lif>
-        <Lif>Container</Lif>
+        <Lif>Images</Lif>
+        <Lif>Containers</Lif>
+        <Lif>And some others magical things</Lif>
       </ul>
     </section>
   );
