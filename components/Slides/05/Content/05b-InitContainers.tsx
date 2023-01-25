0f8811f897beb36844f1f5b741afe93b8d4a31e6
sathoune <michal5bargiel@gmail.com>
2023-01-25 07:07:23 +0100

    💄 Adjust margin.

--- components/Slides/05/Content/05b-InitContainers.tsx
+++ components/Slides/05/Content/05b-InitContainers.tsx
@@ -2,7 +2,7 @@ import { compose } from "./init/init";
 
 const InitContainersB = () => {
   return (
-    <section className={"h-full mt-5"} data-auto-animate>
+    <section className={"h-full mt-20"} data-auto-animate>
       <h2>{"Init Containers"}</h2>
       <pre className={"h-3/5 m-5"}>
         <code data-line-numbers={"12-14,20"}>{compose}</code>
