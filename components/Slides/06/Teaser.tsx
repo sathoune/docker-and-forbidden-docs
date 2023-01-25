316dcd4f0147cb00feaf8857d08ad7d2b0d90c5d
sathoune <michal5bargiel@gmail.com>
2023-01-25 09:03:39 +0100

    ✨ Content to come.

--- components/Slides/06/Teaser.tsx
+++ components/Slides/06/Teaser.tsx
@@ -1,8 +1,14 @@
+import Lif from "../../Commons/Lif";
+
 const Teaser = () => {
   return (
     <section>
-      <h2>This is a title</h2>
-
+      <h2>In the next episode</h2>
+      <ul>
+        <Lif>Build your on CI/CD</Lif>
+        <Lif>Some refactoring</Lif>
+        <Lif>TBD</Lif>
+      </ul>
       <aside className={"notes"}></aside>
     </section>
   );
